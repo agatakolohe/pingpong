99b8153dbcdd529de30ec399d3cf1463d63f586c
agatakolohe <agatakolohe@gmail.com>
2020-12-07 13:57:39 -0800

    create loop and conditional to return a ping pong number range, edit readme
    Co-authored-by: Agata Kolodziej <agatakolohe@gmail.com>
    co-authored-by: Patrick Osten <posten.coding@gmail.com>

---
+++ PingPong.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+public class PingPong
+{
+  static void Main()
+  {
+    Console.WriteLine("Enter an number and I will PingPongify it!");
+    string stringNumber = Console.ReadLine();
+    int userNumber = int.Parse(stringNumber);
+
+    //List<int> pingPongList = new List<int> {};
+
+    for (int index = 1; index <= userNumber; index++)
+    {
+      if (index % 15 == 0)
+      {
+        Console.WriteLine("ping-pong");
+      }
+      else if (index % 5 == 0)
+      {
+        Console.WriteLine("pong");
+      }
+      else if (index % 3 == 0)
+      {
+        Console.WriteLine("ping");
+      }
+      else 
+      {
+        Console.WriteLine(index);
+      }
+    }
+  }
+}
